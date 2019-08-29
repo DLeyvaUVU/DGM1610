a23fec5648d4b4c1f8653bdbd612edd5617b49f5
Damian <10818725@my.uvu.edu>
2019-08-29 15:27:38 -0600

    Created CamControl Script and made a timer
    I was bored and made a timer. It works, but it'd be better if I used a FixedUpdate, but I forgot how to do that and didn't want to learn now.

---
+++ Project 1/Assets/CamControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        print(message:"Hello World");
+        
+    }
+
+    private int Counter;
+    private int passedTime;
+
+    // Update is called once per frame
+    void Update() {
+        Counter++;
+        if (Counter==60) {
+            passedTime++;
+            print(message: passedTime + " seconds have passed...");
+            Counter = 0;
+        }
+    }
+}
