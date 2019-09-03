4a49aac280b4fd96b19e1c2ecdff9bc73e2bd102
Damian Leyva <10818725@my.uvu.edu>
2019-09-02 23:24:07 -0600

    Set up unity for home use | Timer customizable in editor
    I couldn't get the exact version for whatever reason, but close enough.

--- Project 1/Assets/CamControl.cs
+++ Project 1/Assets/CamControl.cs
@@ -7,18 +7,18 @@ public class CamControl : MonoBehaviour
     // Start is called before the first frame update
     private void Start() {
         print(message:"Hello World");
-        
     }
 
     public int Counter; //used for timer
     public int passedTime; //used for timer
-
     // Update is called once per frame
+    public int counterInterval; //timing interval
+    public string intervalType; //name of the interval
     private void Update() {
         Counter++; //increments counter every frame
-        if (Counter==60) { //60 means 60 frames or 1 second has passed
+        if (Counter==counterInterval) { //60 means 60 frames or 1 second has passed
             passedTime++; //increment my timer every scond
-            print(message: passedTime + " seconds have passed..."); //show on the console how much time has passed in seconds
+            print(message: passedTime + " " + intervalType + "s have passed..."); //show on the console how much time has passed in seconds
             Counter = 0; //reset to 0 so we can start counting the new second
         }
     }
