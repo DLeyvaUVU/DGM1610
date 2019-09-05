af100391a6eaa0349157d4126c040dca48106b9c
Damian <10818725@my.uvu.edu>
2019-09-05 15:30:48 -0600

    Created new scene, Made CharacterControl script
    I used a sample script of Anthony Romrell's as a base and changed it to suit my needs.

---
+++ Project 1/Assets/CharacterControl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    //modifying your sample script "SimpleCharacterControl.cs" to fit my needs
+    public Vector2 moveVector;
+    private CharacterController character;
+    public float charSpeed = 2;
+
+    private void Start()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    private void Move(float inputY, float inputX)
+    {
+        moveVector.x = inputX;
+        moveVector.y = inputY;
+        moveVector = transform.TransformDirection(moveVector*charSpeed*Time.deltaTime);
+        character.Move(moveVector);
+    }
+
+    private void Update()
+    {
+        Move(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+    }
+}
