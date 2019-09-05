66f5f2ed070086fdc978c5e3143462fd98f6f81c
Damian <10818725@my.uvu.edu>
2019-09-05 16:40:41 -0600

    Started CameraController script (WIP)
    Had to leave in the middle

---
+++ Project 1/Assets/CameraMovement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Vector3 CamFocus;
+    public CharacterController CameraControl;
+    public GameObject Player;
+    private Vector3 CamPosition;
+    public float SnapSpeed;
+    public float CamSpeed;
+
+    private void Start()
+    {
+        CamFocus = Player.transform.position;
+    }
+
+    public IEnumerator CameraSnap(Vector3 newFocus)
+    {
+        while (CamPosition != newFocus)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, newFocus, SnapSpeed * Time.deltaTime);
+        }
+        CamFocus = newFocus;
+        return null;
+    }
+    private void Update()
+    {
+        CamPosition = transform.position;
+        /*if (CamFocus == Player.transform.position)
+        {
+            
+        }*/
+    }
+}
--- Project 1/Assets/CharacterControl.cs
+++ Project 1/Assets/CharacterControl.cs
@@ -6,25 +6,33 @@ using UnityEngine;
 public class CharacterControl : MonoBehaviour
 {
     //modifying your sample script "SimpleCharacterControl.cs" to fit my needs
-    public Vector2 moveVector;
+    private Vector2 moveVector;
     private CharacterController character;
+    public GameObject Camera;
     public float charSpeed = 2;
 
     private void Start()
     {
         character = GetComponent<CharacterController>();
+        //I can technically make this public and just set it in the editor instead of using this code
     }
 
-    private void Move(float inputY, float inputX)
+    private void Move(float inputY, float inputX)//need two axes to move in a 2d space
     {
         moveVector.x = inputX;
         moveVector.y = inputY;
-        moveVector = transform.TransformDirection(moveVector*charSpeed*Time.deltaTime);
-        character.Move(moveVector);
+        moveVector = moveVector*charSpeed*Time.deltaTime;
+        character.Move(moveVector);//wasn't sure if this function accepted a Vector2 but it works
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Camera.CameraSnap(other.transform.position);
     }
 
     private void Update()
     {
+        //gets two inputs to actually move across two axes
         Move(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
     }
 }
