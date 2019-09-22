eb5d7a4c0dcd61e3c4bf007416add8dc07d93ce3
Damian Leyva <10818725@my.uvu.edu>
2019-09-21 22:45:45 -0600

    Implemented Run
    Also started work a special fire

--- Project 1/Assets/2dPlatformerScripts/cController.cs
+++ Project 1/Assets/2dPlatformerScripts/cController.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class cController : MonoBehaviour {
     public CharacterController player;
-    public float terminalVelocity, gravity = 9.81f, jumpVector, speedVector;
+    public float terminalVelocity, gravity = 9.81f, jumpVector, speedVector, charSpeed, runSpeed;
     private Vector3 movementVector;
     private int jumpCount;
 
@@ -27,15 +27,30 @@ public class cController : MonoBehaviour {
         player.Move(movementVector * Time.deltaTime);
     }
 
+    public IEnumerator BulletTimeFire(Vector3 initialMovement) {
+        while (Input.GetButton("Fire2")) {
+            yield return null;
+        }
+        movementVector = initialMovement;
+    }
     private void Update() {
         Move(Input.GetAxis("Horizontal"));
         if (Input.GetButtonDown("Jump") && jumpCount < 1) {
             movementVector.y = jumpVector;
             jumpCount++;
         }
-
+        if (Input.GetButton("Run")) {
+            speedVector = runSpeed;
+        }
+        else {
+            speedVector = charSpeed;
+        }
         if (player.isGrounded) {
             jumpCount = 0;
         }
+
+        // if (Input.GetButtonDown("Fire2")) {
+        //     StartCoroutine(BulletTimeFire(movementVector));
+        // }
     }
 }
