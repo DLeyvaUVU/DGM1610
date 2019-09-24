f7ffb03fef668d6b647af8f79c14002e59be8ba4
Damian Leyva <10818725@my.uvu.edu>
2019-09-23 23:24:02 -0600

    Implemented a busy function to lock out movement in certain situations
    also more work on the "bullet time" effect, formerly "special fire"

--- Project 1/Assets/2dPlatformerScripts/cController.cs
+++ Project 1/Assets/2dPlatformerScripts/cController.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class cController : MonoBehaviour {
     public CharacterController player;
     public float terminalVelocity, gravity = 9.81f, jumpVector, speedVector, charSpeed, runSpeed;
+    private bool busy = false;
     private Vector3 movementVector;
     private int jumpCount;
 
@@ -29,12 +30,18 @@ public class cController : MonoBehaviour {
 
     public IEnumerator BulletTimeFire(Vector3 initialMovement) {
         while (Input.GetButton("Fire2")) {
+            movementVector *= 0;
             yield return null;
         }
         movementVector = initialMovement;
+        busy = false;
+        print("shoot projectile in direction of mouse or left stick on controller");
+        //at this point I would shoot something, but I'm not quite sure how to do that yet.
     }
     private void Update() {
-        Move(Input.GetAxis("Horizontal"));
+        if (!busy) {
+            Move(Input.GetAxis("Horizontal"));
+        }
         if (Input.GetButtonDown("Jump") && jumpCount < 1) {
             movementVector.y = jumpVector;
             jumpCount++;
@@ -49,8 +56,9 @@ public class cController : MonoBehaviour {
             jumpCount = 0;
         }
 
-        // if (Input.GetButtonDown("Fire2")) {
-        //     StartCoroutine(BulletTimeFire(movementVector));
-        // }
+        if (Input.GetButtonDown("Fire2") && !player.isGrounded) {
+            busy = true;
+            StartCoroutine(BulletTimeFire(movementVector));
+        }
     }
 }
