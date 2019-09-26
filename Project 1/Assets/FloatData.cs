36a2e46cfc1a6698a6d79733e41053fb3fd7cc54
Damian Leyva <10818725@my.uvu.edu>
2019-09-25 20:35:39 -0600

    Reset game when hp goes to 0, Fixed fruit check

--- Project 1/Assets/FloatData.cs
+++ Project 1/Assets/FloatData.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 [CreateAssetMenu]
 public class FloatData : ScriptableObject
 {
@@ -10,8 +12,9 @@ public class FloatData : ScriptableObject
 
     public void AdjustValue(int input) {
         currentValue += input;
-        if (currentValue < 0) {
-            currentValue = 0;
+        if (currentValue <= 0) {
+            currentValue = maxValue;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
         magnitude = currentValue / maxValue;
     }
--- Project 1/Assets/GetFruit.cs
+++ Project 1/Assets/GetFruit.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 
 public class GetFruit : MonoBehaviour {
     public string[] fruit = {"Apple", "Orange", "Peach"};
-    public bool[] ripe = {false, false, false, true};
+    public bool[] ripe = {false, false, false};
     private int i = 0;
 
     private void Start() {
         i = 0;
-        while (!ripe[i]) {
+        bool exitLoop = false;
+        while (!ripe[i] && !exitLoop) {
             print("There isn't a ripe " + fruit[i] + "?!");
-            i++;
-            if (i < fruit.Length) {
+            if (i + 1 < fruit.Length) {
+                i++;
                 print("Go check for a ripe " + fruit[i] + ".");
             }
             else {
                 print("Well just come home then...");
+                exitLoop = true;
             }
         }
     }
