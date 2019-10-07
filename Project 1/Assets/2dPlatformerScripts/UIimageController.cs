7972b79b2cf93cca341c8aec88b5a3951e34780b
Damian Leyva <10818725@my.uvu.edu>
2019-10-06 23:51:15 -0600

    Added death timer for projectiles, Started some code for fill animation

--- Project 1/Assets/2dPlatformerScripts/UIimageController.cs
+++ Project 1/Assets/2dPlatformerScripts/UIimageController.cs
@@ -6,11 +6,23 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Image))]
 public class UIimageController : MonoBehaviour {
     private Image img;
+    private Coroutine animCoroutine;
+    private float animDirection = 1;
     private void Awake() {
         img = GetComponent<Image>();
     }
+    private IEnumerator animateFill(float magnitude) {
+        // animDirection = (magnitude - img.fillAmount >= 0) ? 1 : -1;
+        // for (float distance = animDirection * (magnitude - img.fillAmount); distance > 0) {
 
+        // }
+        yield return null;
+    }
     public void UpdateImage(FloatData data) {
-        img.fillAmount = data.magnitude;
+        if (animCoroutine != null) {
+            StopCoroutine(animCoroutine);
+        }
+        animCoroutine = StartCoroutine(animateFill(data.magnitude));
+        //img.fillAmount = data.magnitude;
     }
 }
--- Project 1/Assets/2dPlatformerScripts/projectileControl.cs
+++ Project 1/Assets/2dPlatformerScripts/projectileControl.cs
@@ -6,8 +6,7 @@ using UnityEngine;
 public class projectileControl : MonoBehaviour
 {
     public varData variableSet;
-    private float power;
-    private float speed;
+    private float power, speed, lifeTime;
     private bool pierce;
     private int bounce;
     public Vector3 directionVector;
@@ -15,14 +14,15 @@ public class projectileControl : MonoBehaviour
     private void Start() {
         power = variableSet.floatValue[0];
         speed = variableSet.floatValue[1];
+        lifeTime = variableSet.floatValue[2];
         pierce = variableSet.boolValue[0];
         bounce = variableSet.intValue[0];
+        Destroy(gameObject, lifeTime);
     }
 
     public void SetDirection(Vector2 newDirection) {
         directionVector = newDirection;
     }
-
     private void Update() {
         transform.Translate(speed * Time.deltaTime * directionVector);
     }
