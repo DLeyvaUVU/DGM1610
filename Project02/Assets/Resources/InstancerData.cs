c8d7aaba083d14bb77745b852f0d8f32a1fe0d62
Damian Leyva <10818725@my.uvu.edu>
2019-10-26 23:32:04 -0600

    Made Player get instantiated. Also implemented unityaction.

---
+++ Project02/Assets/Resources/InstancerData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InstancerData : ScriptableObject {
+    public UnityAction<GameObject> instanceAction;
+    public GameArtData artData;
+    public GameObject prefab;
+
+    public void InstantiateObject(Vector3 instancePosition) {
+        var objectInstance = Instantiate(prefab);
+        objectInstance.transform.position = instancePosition;
+        instanceAction(objectInstance);
+    }
+}
--- Project02/Assets/Scripts/GameController.cs
+++ Project02/Assets/Scripts/GameController.cs
@@ -1,7 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameController : StartEvent {
     public GameObject mainCam;
+    public PlayerData selectedPlayer;
+    public GameObject playerInstance;
+    private void Awake() {
+        selectedPlayer.instanceAction = SetPlayer;
+        selectedPlayer.InstantiateObject(transform.position);
+    }
+    private void SetPlayer(GameObject obj) {
+        playerInstance = obj;
+        var camScript = mainCam.GetComponent<SnapMovement>();
+        camScript.SetTarget(playerInstance.transform);
+    }
 }
