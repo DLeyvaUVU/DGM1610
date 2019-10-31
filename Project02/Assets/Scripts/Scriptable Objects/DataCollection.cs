b9bf35cacb130103e54f4cf4a78720b58e61f1ae
Damian Leyva <10818725@my.uvu.edu>
2019-10-31 00:03:21 -0600

    Made my collection have multiple lists for subweapons and collectibles
    also reorganized folders

---
+++ Project02/Assets/Scripts/Scriptable Objects/DataCollection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu]
+public class DataCollection : ScriptableObject {
+    public enum ListType {
+        Collectible, Subweapon
+    }
+    public List<Collectible> collectibleList;
+    public List<Subweapon> subweaponList;
+    
+
+    public void ClearList(int selection) {
+        ListType listSelection = (ListType)selection;
+        int i = 0;
+        switch (listSelection) {
+            case ListType.Collectible:
+                i = collectibleList.Count;
+                collectibleList.RemoveRange(0, i);
+                break;
+            case ListType.Subweapon:
+                i = subweaponList.Count;
+                subweaponList.RemoveRange(0, i);
+                break;
+        }
+    }
+
+    public void AddData(Collectible newCollectible) {
+        if (newCollectible.isUnique) {
+            collectibleList.Add(newCollectible);
+        }
+        if (!collectibleList.Contains(newCollectible)) {
+            collectibleList.Add(newCollectible);
+        }
+        collectibleList.Sort();
+        newCollectible.count++;
+    }
+
+    public void AddData(Subweapon newSubweapon) {
+        if (!subweaponList.Contains(newSubweapon)) {
+            subweaponList.Add(newSubweapon);
+        }
+        subweaponList.Sort();
+    }
+}
